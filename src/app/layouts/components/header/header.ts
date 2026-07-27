4bd7d5308ca1e891f764f35949850e3f3e0a6a52
Amado Melguizo <amado.melgizo@gmail.com>
2026-07-27 02:00:10 +0200

    Fix: mobile header menu

--- src/app/layouts/components/header/header.ts
+++ src/app/layouts/components/header/header.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [
-    RouterLink
-  ],
+  imports: [RouterLink,CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
-export class Header {}
+export class Header {
+
+  mobileMenuOpen = false;
+
+  galleryOpen = false;
+
+  toggleMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+
+    if (!this.mobileMenuOpen) {
+      this.galleryOpen = false;
+    }
+  }
+
+  closeMenu() {
+    this.mobileMenuOpen = false;
+    this.galleryOpen = false;
+  }
+
+  toggleGallery() {
+    this.galleryOpen = !this.galleryOpen;
+  }
+
+}
